19b4cb2608aa6baf94558dbbca67c7fe7ab5ebe7
xygodcyx <1323943635@qq.com>
2024-06-23 18:22:30 +0800

    chore css

--- src/pages/userInfo/index.vue
+++ src/pages/userInfo/index.vue
@@ -34,36 +34,7 @@ onMounted(() => {
 </script>
 
 <template>
-  <div flex="~ justify-center items-center" transform>
-    <div wa>
-      <span mb3 block text-4xl>Hi :</span>
-      <div flex="~ justify-start items-center gap2">
-        <img :src="userStore.userInfo.imgFace" border-rounded>
-        <div>
-          <span text-4xl text-blue>{{ userStore.userInfo.name }}</span>
-          <span ml2 text-sm> {{ hi }}</span>
-        </div>
-      </div>
-      <p mb3 mt3 text-3xl text-amber>
-        干点什么?
-      </p>
-      <div flex="~ gap2" h7 w160>
-        <div h-full text-3 btn @click="router.push('/cart')">
-          去购物车看看
-        </div>
-        <div h-full text-3 btn @click="router.push('/like')">
-          去收藏看看
-        </div>
-        <div h-full text-3 btn @click="router.push('/order')">
-          去订单看看
-        </div>
-      </div>
-      <p mb3 mt3 text-3xl text-amber>
-        我有什么?
-      </p>
-      <div class="userInfo" flex="~ gap1">
-        <span text-5>钱: </span><span text-2xl text-red>{{ userStore.userInfo.balance }}</span>
-      </div>
-    </div>
+  <div flex="~ justify-center items-center">
+    <h1>Hi</h1>
   </div>
 </template>
