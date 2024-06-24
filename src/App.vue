be3e3f7458498db83f761b7cba30254e29f46195
xygodcyx <1323943635@qq.com>
2024-06-24 21:28:25 +0800

    add Transition

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,34 @@
   <main p-4 font-sans text="gray-700 dark:gray-200">
     <TheTopNav />
     <TheBanner />
-    <RouterView />
+    <router-view v-slot="{ Component }">
+      <Transition name="slide-fade">
+        <component :is="Component" />
+      </Transition>
+    </router-view>
+    <!-- <Transition>
+      <RouterView />
+    </Transition> -->
   </main>
 </template>
+
+<style>
+/*
+  进入和离开动画可以使用不同
+  持续时间和速度曲线。
+*/
+.slide-fade-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.slide-fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translateX(-20px);
+  transition: none;
+  opacity: 0;
+}
+</style>
--- src/pages/shop/index.vue
+++ src/pages/shop/index.vue
@@ -46,7 +46,7 @@ async function buyAllShopGood() {
 </script>
 
 <template>
-  <div class="shopGoodsWrap" flex="~ justify-center items-center" font-sans>
+  <div class="shopGoodsWrap" flex="~ justify-center items-center" mt--6 font-sans>
     <div v-if="shopGoods?.length !== 0" class="shopGoods" flex="col">
       <!-- operation -->
       <div flex="~ justify-start items-center gap2" mb2>
@@ -62,18 +62,21 @@ async function buyAllShopGood() {
       </div>
       <div
         v-for="shopGood in shopGoods" :key="shopGood.id" class="shopGoodItem"
-        mb4
-        h10 flex="~ items-center justify-between gap6" lt-sm="gap0"
+
+        mb4 h10 w86 b-rounded shadow shadow-amber flex="~ items-center justify-between gap6" lt-sm="gap0"
       >
         <div class="left" flex="~ justify-center items-center gap4" lt-sm="h10 gap1" h20>
-          <img :src="shopGood.good.pic.replace(shopGood.good.label.substring(0, 5), shopGood.good.label[0]).replace('600x400', '30x30')" alt="12" lt-sm="h7 w7" inline-block h10 w10 b-rounded>
-          <span mr3 lt-sm="text-3" text-2xl :title="shopGood.good.label">{{ shopGood.good.label.length > 12 ? `${shopGood.good.label.substring(0, 12)}...` : shopGood.good.label }}</span>
-          <div flex="~ items-center justify-center" wa>
-            <span mr2 lt-sm="text-4" text-2xl :title="shopGood.good.label">{{ shopGood.quantity }}</span>
-            <span text-5 text-red lt-sm="text-4" :title="shopGood.good.label">￥{{ shopGood.sum }}</span>
+          <img
+            ml1
+            :src="shopGood.good.pic.replace(shopGood.good.label.substring(0, 5), shopGood.good.label[0]).replace('600x400', '30x30')" alt="12" lt-sm="h7 w7" inline-block h10 w10 b-rounded
+          >
+          <span lt-sm="text-3" ml1 mr2 w22 overflow-hidden text-ellipsis text-nowrap text-2xl :title="shopGood.good.label">{{ shopGood.good.label }}</span>
+          <div flex="~ items-center justify-start" w20>
+            <span mr1 text-start lt-sm="text-4" text-2xl :title="shopGood.good.label">{{ shopGood.quantity }}</span>
+            <span text-start text-5 text-red flex="2" lt-sm="text-4" :title="shopGood.good.label">￥{{ shopGood.sum }}</span>
           </div>
         </div>
-        <div class="right" flex="~ justify-between items-center gap1" mr2 lt-sm="ml4 mr0">
+        <div class="right" flex="~ justify-between items-center gap1" mr2 lt-sm="ml4 mr2">
           <span text-btn h10 w10 lh-10 lt-sm="h6 w6 lh-6" @click="removeShopGood(shopGood.id)">移</span>
           <span text-btn h10 w10 bg-green lh-10 lt-sm="h6 w6 lh-6" @click="increaseShopGoodQuantity(shopGood)">加</span>
           <span text-btn h10 w10 bg-amber lh-10 lt-sm="h6 w6 lh-6" @click="subtractShopGoodQuantity(shopGood)">减</span>
